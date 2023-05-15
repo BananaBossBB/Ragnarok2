c56072d1bdb6315c1bfc47554eaafa9c8a10f821
badarzaa <104325948+BananaBossBB@users.noreply.github.com>
2023-05-15 16:04:55 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program 
+{    
+    static void Main(string[] args) 
+    {
+        int city = int.Parse(Console.ReadLine());
+        string[] cityName = new string[city];
+        int[] covidLevel = new int[city];
+        int[] cityNum = new int[city];
+
+        for(int i = 0;i < city; i++)
+        {
+            cityNum[i] = i;
+            cityName[i] = Console.ReadLine();
+            int cityCommu = int.Parse(Console.ReadLine());
+            
+            for(int l = 0;l <= cityCommu;l++)
+            {    
+                int cityCommuNum = int.Parse(Console.ReadLine());
+                if(cityCommuNum > city)
+                {
+                    Console.WriteLine("Invalid ID");
+                }
+                else
+                {
+                }
+            }
+        }   
+        Console.WriteLine(cityName[0],covidLevel); //test
+        Console.WriteLine(cityName[1],covidLevel);
+        Console.WriteLine(cityName[2],covidLevel);
+    }
+}
